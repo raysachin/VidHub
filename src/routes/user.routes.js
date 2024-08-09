65035c135c033dab51f3c48864f45d6c68addb39
raysachin <raysachin2002@gmail.com>
2024-08-10 01:11:02 +0530

    Add new routes for user controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -28,6 +28,29 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser);
 
 router.route("/refresh-token").post(refreshAccessToken)  // yha p verify jwt nii lgana pda kyuki hmne wo sab kaam controller me hi kr diya
+
+// router to change password
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+// Router for current user
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+// Route for update account details
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)  // patch because all details should not be updated when request to change one details, so we will not use post
+
+// Routes for update avatar
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+// router for cover Image
+router.route("/update-cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+
+// Router for channel profile
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+// Router for watch history
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
+
+
 
 
 
